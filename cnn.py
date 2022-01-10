8e2e49f7088b9ca98e8e14b89271e8431a62250a
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-10 14:27:48 +0800

    setting up the cnn model

---
+++ cnn.py
@@ -0,0 +1,25 @@
+# This Python 3 environment comes with many helpful anal,ytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+from keras import backend as K
+
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "./input"]).decode("utf8"))
+
+
+# Any results you write to the current directory are saved as output.
+
